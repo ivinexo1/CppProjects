a861106c3d4d48574b9cee58b8186d57160ed2f7
MobilePc <ivinexoiot@gmail.com>
2024-05-29 19:24:27 +0200

    i duno

--- bubble/bubble.cpp
+++ bubble/bubble.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include "/home/ivinexo/lib/c++/arr/arr.cpp"
 
 
 int swap(int * a, int * b){
@@ -13,6 +13,8 @@ int swap(int * a, int * b){
 }
     
 int main() {
+    array array;
+
     int arr[] = { 432, 342, 34, 234, 3, 213, 367, 847, 763 };
     int i, j;
     int n = sizeof(arr) / sizeof(arr[0]);
@@ -26,7 +28,7 @@ int main() {
     for( j = 0; n > j; j++) {
         for(i = 0; n > i; i++) {
             if (arr[i] > arr[i + 1]) {
-                swap(&arr[i], &arr[i + 1]); 
+                array.swap(&arr[i], &arr[i + 1]); 
             }
         }
     }

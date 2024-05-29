e28e42f5c92b100ebb7890090afdc6b138aec4e0
MobilePc <ivinexoiot@gmail.com>
2024-05-29 21:10:03 +0200

    addet project

---
+++ calculator/calcu.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char comm;
+    int a, b;
+
+    cout << "num 1, command, num 2 \n";
+    cin >> a >> comm >> b;
+
+    do {
+        swap(comm) {
+            case '+' 

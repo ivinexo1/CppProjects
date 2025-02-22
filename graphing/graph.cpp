618a0cc1913e14171e1557f33249afdd0dfb49a5
MobilePc <ivinexoiot@gmail.com>
2025-02-22 20:50:59 +0100

    graphing calculator progress

---
+++ graphing/graph.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    double y = 10;
+    double x;
+    for( ; y >= -10; y = y - 0.5){
+//        std::cout << " " <<  y << " ";
+        for ( x = -10; x <= 10; x = x + 0.5) {
+            if( (2 * x) == y){
+                std::cout << " # ";
+            } else {
+                std::cout << " . ";
+            }
+        }
+        std::cout << std::endl;
+    }
+//    for (double i = -1; i <= 1; i = i + 0.1){
+//        std::cout << i;
+//    }
+
+    return 0;
+}

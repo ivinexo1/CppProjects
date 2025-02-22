a12efd7a603758b567083d7a81204d8207c5a7e2
MobilePc <ivinexoiot@gmail.com>
2025-02-22 20:56:35 +0100

    Added second equation to graphing

--- graphing/graph.cpp
+++ graphing/graph.cpp
@@ -3,12 +3,15 @@
 int main() {
     double y = 10;
     double x;
+
     for( ; y >= -10; y = y - 0.5){
 //        std::cout << " " <<  y << " ";
         for ( x = -10; x <= 10; x = x + 0.5) {
-            if( (2 * x) == y){
+            if( (0.5 * x) == y){
                 std::cout << " # ";
-            } else {
+            } else if ((3 * (x+2)) == y){
+                std::cout << " # ";
+            }else {
                 std::cout << " . ";
             }
         }

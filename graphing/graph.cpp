c81d0c0f17174890dedd86bf43875e5786f2539d
MobilePc <ivinexoiot@gmail.com>
2025-02-24 21:27:57 +0100

    Added a aproxiamtion of cube root and numbering rows and colmuns to calcualtor

--- graphing/graph.cpp
+++ graphing/graph.cpp
@@ -5,13 +5,44 @@ int main() {
     math math;
     double y = 20;
     double x;
+    
+    std::cout << "   ";
+    for(x = -20; x <= 20; x++) {
+        if(x < 0) {
+            if(x > -10) {
+                std::cout << x << " ";
+            } else {
+                std::cout << x;
+            }
+        } else {
+            if(x < 10) { 
+                std::cout << " " << x << " ";
+            } else {
+                std::cout << " " << x;
+            }
+        }
+    }
+    std::cout << "\n";
 
     for( ; y >= -20; y--){
+        if(y < 0) {
+            if(y > -10) {
+                std::cout << y << " ";
+            } else {
+                std::cout << y;
+            }
+        } else {
+            if(y < 10) { 
+                std::cout << " " << y << " ";
+            } else {
+                std::cout << " " << y;
+            }
+        }
 //        std::cout << " " <<  y << " ";
         for ( x = -20; x <= 20; x++) {
-            if( math.intExp(2, x) == y){
+            if( x + y == 5){
                 std::cout << " # ";
-//            } else if (((3+x) * (x+2)) == y){
+//            } else if (math.intExp(2,x) == y){
 //                std::cout << " & ";
             }else {
                 std::cout << " . ";
@@ -22,6 +53,6 @@ int main() {
 //    for (double i = -1; i <= 1; i = i + 0.1){
 //        std::cout << i;
 //    }
-
+    std::cout << "\n" << math.doSqrt(2) << "\n" << math.doCube(16) << std::endl;
     return 0;
 }
--- math/math.cpp
+++ math/math.cpp
@@ -1,9 +1,41 @@
 class math {
     public:
-    int intExp(double base, int expo) {
+    double intExp(double base, int expo) {
         int out = 1;
-        for (int i = 1; i <= expo; i++) {
+        for(int i = 1; i <= expo; i++) {
             out = out * base;
+        }
+        return out;
+    }
+    int intSqrt(int x) {
+        int i = 1;
+
+        while(i * i <= x){
+            i++;
+        }
+        return i--;
+    }
+    double doSqrt(double x){
+        double out = (1 + x/1)/2;
+        for(int i = 0; i <= 10000000; i++){
+            out = (out + (x / out)) / 2;
+        }
+
+        return out;
+    }
+    int intCube(int x) {
+        int i = 1;
+        while(i * i * i <= x) {
+            i++;
+        }
+        return i--;
+
+    }
+    double doCube(int x) {
+        double out = 1;
+
+        for(int i = 1; i <= 100; i++){
+            out = ((x / out) + (2 * out)) / 3;
         }
         return out;
     }

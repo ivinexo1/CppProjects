44075436d923d13b8b9d03a6d9acee673decd3b0
ivinexo <ivinexo@Ivinexo.arch>
2025-05-31 16:50:32 +0200

    Playing with opengl

--- Gtk/base.cc
+++ Gtk/base.cc
@@ -8,48 +8,67 @@ class MyWindow : public Gtk::Window
 public:
   Gtk::ToggleButton m_Button;
   MyWindow();
-  void on_button_clicked();
+  void on_button_clicked(std::string, std::string, std::string);
+  bool if_in_file(std::string, std::string);
 };
 
 MyWindow::MyWindow()
 {
   Gtk::Fixed fixed;
-  auto label = Gtk::make_managed<Gtk::Label>("");
- /* Set some window properties */
   set_title("Frame Example");
   set_size_request(300, 300);
+
   fixed.put(m_Button, 140, 140);
   m_Button.set_size_request(20, 20);
-  m_Button.signal_clicked().connect( sigc::mem_fun(*this, &MyWindow::on_button_clicked) );
-  /* Sets the margin around the frame. */
-  m_Button.set_child(*label); 
+  if(if_in_file("hello", "true")) {
+        m_Button.set_active();
+  }
+  m_Button.signal_clicked().connect( sigc::bind( sigc::mem_fun(*this, &MyWindow::on_button_clicked), "hello", "true", "false"));
+
   set_child(fixed);
 }
 
-void MyWindow::on_button_clicked()
+bool MyWindow::if_in_file(std::string fname, std::string string){
+    std::string file;
+
+    // Read from the text file
+    std::ifstream MyReadFile(fname);
+    std::stringstream buffer;
+    buffer << MyReadFile.rdbuf();
+    MyReadFile.close(); 
+    file = buffer.str();
+    std::size_t pos = file.find(string);
+    if (pos != std::string::npos){
+        return true;
+    }
+    return false;
+}
+
+void MyWindow::on_button_clicked(std::string fname, std::string string, std::string replace)
 {
     std::string file;
      // Read from the text file
-    std::ifstream MyReadFile("hello");
+    std::ifstream MyReadFile(fname);
     std::stringstream buffer;
     buffer << MyReadFile.rdbuf();
     MyReadFile.close(); 
     file = buffer.str();
     if(m_Button.get_active()){
-        std::size_t pos = file.find("false");
+        std::size_t pos = file.find(replace);
         if (pos != std::string::npos){
-            file.replace(pos, 5, "true");
+            file.replace(pos, 5, string);
         }
     }else{
-        std::size_t pos = file.find("true");
+        std::size_t pos = file.find(string);
         if (pos != std::string::npos){
-            file.replace(pos, 4, "false");
+            file.replace(pos, 4, replace);
         }
     }
     std::ofstream Write("hello");
     Write << file;
     Write.close();
     std::cout << file;
+    return;
 }
 
 int main(int argc, char* argv[])
